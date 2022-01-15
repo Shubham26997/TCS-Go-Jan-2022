eb0e71fc77ff243bb926af5002bd3e48fa174249
Shubham26997 <shubham.goel386@gmail.com>
2022-01-15 12:35:10 +0530

    Hello world program

---
+++ hello_world.go
@@ -0,0 +1,11 @@
+// package declare
+package main
+
+// import SDK
+import "fmt"
+
+// Declare the function using "func" keyword
+func main() {
+	// methods are case sensitive so use as declare in the "pkg.go.dev/std"
+	fmt.Println("Hello world")
+}
